f5a4d4b398657dc796d7fb9ff58cccaacb84a58d
ayu2201010551 <127836672+ayu2201010551@users.noreply.github.com>
2023-03-14 17:01:43 +0800

    Sesi1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan HTML Sesi 1</title>
+</head>
+<body>
+    
+    <h3> Mari Belajar Web Programing <?="<sup>80</sup>"?></h3>
+
+    <script>
+        document.write("<h3>Belajar menggunakan Javascript</h3>");
+    </script>
+
+<?php
+   echo "<h3>Menggunakan script PHP</h3>";
+?>
+
+</body>
+</html>
